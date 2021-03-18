300aca3f78c8cd9d2e6dce35f1408a822c74aa2e
Epic91 <hernandeze791@gmail.com>
2021-03-18 18:43:02 -0500

    added instance variables and added validation on Users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :entries
+
+    validates :username, presence: true, uniqueness: true, length: {in: 5..15}
+    validates :password, presence: true, length: {in: 5..10}
+    validates :email, presence: true, uniqueness: true
 end
