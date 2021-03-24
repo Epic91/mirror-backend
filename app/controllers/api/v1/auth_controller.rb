fbbe8b7b2d8c6e7e94d02c0dd5c6df44bcfd3e0c
Evelyn Hernandez <hernandeze791@gmail.com>
2021-03-24 14:14:08 -0500

    added auth controller and set up entries and user routes

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,4 +1,20 @@
-class Api::v1::AuthController < ApplicationController
+class Api::V1::AuthController < ApplicationController
     def create
+        user =  User.find_by(username: params[:username])
+        
+        if user && user.authenticate(params[:password])
+            render json: user
+        else
+            render json: {error: 'Invalid username or password'}
+        end
     end
 end
+# find the user that has been provided in the form
+# if the user exists:
+#   -verify that password entered in the frontend, exists in the backend
+#   if matches:
+#       -success!
+#       -render user that just logged in
+#   else:
+#       errors
+# else validation errors
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :entries
+  get '/entries', to: 'entries#index'
+
   resources :prompts
 
-  namespace :api do 
+  namespace :api do
     namespace :v1 do
       resources :users
       post '/auth', to:'auth#create'
+
     end
   end
+
 end
