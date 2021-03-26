170d8a06618ce5406e204048d641f00017a8518d
Evelyn Hernandez <hernandeze791@gmail.com>
2021-03-26 01:29:08 -0500

    finished login auth web tokens

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,13 +6,26 @@ class Api::V1::AuthController < ApplicationController
             payload = {user_id: user.id }
             hmac_secret = 'S3CR3T'
 
-
             token = JWT.encode(payload, hmac_secret, 'HS256')
 
             #create a token and hide the ussers id
             render json: {user: UserSerializer.new(user), token: token}
         else
             render json: {error: 'Invalid username or password'}
+        end
+    end
+
+    def show
+        token = request.headers[:Authorization].split(' ')[1]
+        decoded_token = JWT.decode(token, 'S3CR3T', true, { algorithm: 'HS256' })
+        user_id = decoded_token[0]['user_id']
+
+        user = User.find(user_id)
+
+        if user
+            render json:  user
+        else
+            render json: {error: 'Invalid token'}
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       post '/auth', to:'auth#create'
+      get'/current_user', to:'auth#show'
 
     end
   end
