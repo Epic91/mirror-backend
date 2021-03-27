67e4f124d4cd013dd77a52898362ffbae3e281c8
Evelyn Hernandez <hernandeze791@gmail.com>
2021-03-27 17:00:57 -0500

    only the login has redux

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   resources :entries
-  # post '/entries', to: 'entries#create'
+  post '/entries', to: 'entries#create'
 
   resources :prompts
 
   namespace :api do
     namespace :v1 do
       resources :users
+      
       post '/auth', to:'auth#create'
-      get'/current_user', to:'auth#show'
+      # get'/current_user', to:'auth#show'
 
     end
   end
